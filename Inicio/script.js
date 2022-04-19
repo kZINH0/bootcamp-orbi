94d9b35534a07bf588b696b75561ef51a6a21d5a
kZINH0 <psoares.renan@gmail.com>
2022-04-19 05:17:06 -0300

    Javascript - Set e Map

---
+++ Inicio/script.js
@@ -0,0 +1,23 @@
+function getAdmins(map) {
+    let admins = [];
+    for ([key, value] of map) {
+        if (value === 'Admin') {
+            admins.push(key)
+        }
+    }
+    return admins;
+}
+
+const usuarios = new map();
+
+usuarios.set('Renan', 'Admin')
+usuarios.set('Lucas', 'RH')
+usuarios.set('Cassio', 'Helpdesk')
+usuarios.set('Vinicius', 'IT')
+usuarios.set('Andre', 'NOC')
+usuarios.set('Bogo', 'Ex')
+usuarios.set('Cesar', 'New')
+usuarios.set('Rene', 'Admin')
+usuarios.set('Francine', 'Manager')
+
+console.log(getAdmins(usuarios));
